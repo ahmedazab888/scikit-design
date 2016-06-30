612b91d800c3cd21f4352695fc6d916a1685e926
Christopher Louden <loudenconsulting@gmail.com>
2016-06-30 09:05:34 -0500

    Copied setup.py from randomization
    There was an error about an index out of the range of a tuple. In
    order to proceed while I figure that out, I used a less modular
    setup.py.

--- setup.py
+++ setup.py
@@ -1,67 +1,25 @@
-#!/usr/bin/env python
-import os
-import sys
-import subprocess
+""" Package Setup for randomization """
+
 from setuptools import setup
 
-descr = """\
-Scikit-Design
-
-Tools for statistical study design for clinical trials, qualtiy control, etc.
-"""
-
-DISTNAME            = 'scikit-learn'
-DESCRIPTION         = 'Tools for Statistical Study Design'
-LONG_DESCRIPTION    = descr
-MAINTAINER          = 'Christopher Louden',
-MAINTAINER_EMAIL    = 'loudenconsulting@gmail.com',
-URL                 = 'http://github.com/louden/scikit-design'
-LICENSE             = 'MIT'
-DOWNLOAD_URL        = URL
-PACKAGE_NAME        = 'skdesign'
-EXTRA_INFO          = dict(
-    install_requires=['scipy'],
-    classifiers=['Development Status :: 1 - Planning',
-                 'Intended Audience :: Developers',
-                 'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: BSD License',
-                 'Topic :: Scientific/Engineering']
-)
+import skdesign
 
 
-def get_version():
-    """Obtain the version number"""
-    import imp
-    mod = imp.load_source('version', os.path.join(PACKAGE_NAME, 'version.py'))
-    return mod.__version__
+def readme():
+    """ Helper function to open the readme file """
+    with open('README.md') as readme_file:
+        return readme_file.read()
 
-# Documentation building command
-try:
-    from sphinx.setup_command import BuildDoc as SphinxBuildDoc
-
-    class BuildDoc(SphinxBuildDoc):
-        """Run in-place build before Sphinx doc build"""
-        def run(self):
-            ret = subprocess.call([sys.executable, sys.argv[0],
-                                   'build_ext', '-i'])
-            if ret != 0:
-                raise RuntimeError("Building Scipy failed!")
-            SphinxBuildDoc.run(self)
-    cmdclass = {'build_sphinx': BuildDoc}
-except ImportError:
-    cmdclass = {}
-
-# Call the setup function
-if __name__ == "__main__":
-    setup(name=DISTNAME,
-          maintainer=MAINTAINER,
-          maintainer_email=MAINTAINER_EMAIL,
-          description=DESCRIPTION,
-          license=LICENSE,
-          url=URL,
-          download_url=DOWNLOAD_URL,
-          long_description=LONG_DESCRIPTION,
-          include_package_data=True,
-          test_suite="py.test",
-          # cmdclass=cmdclass,
-          version=get_version())
+# Check this against the documentation for setuptools
+setup(name='skdesign',
+      version='1.0.0',
+      description='Tools for Statistical Study Design',
+      long_description=readme(),
+      keywords='randomization study design trial',
+      url='http://github.com/louden/skdesign',
+      author='Christopher Louden',
+      author_email='chris@loudenanalytics.com',
+      license='BSD',
+      packages=['skdesign'],
+      install_requires=[],
+      test_suite='py.test')
