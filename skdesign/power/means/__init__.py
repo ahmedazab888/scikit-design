bef9e7348af88090a041f3977b41b7f7ad48f766
Christopher Louden <loudenconsulting@gmail.com>
2016-07-05 16:46:41 -0500

    Add means.TwoSampleParallel

--- skdesign/power/means/__init__.py
+++ skdesign/power/means/__init__.py
@@ -1,2 +1,3 @@
 from .means_base import MeansBase
 from .one_sample import OneSample
+from .two_sample_parallel import TwoSampleParallel
--- tests/test_means.py
+++ tests/test_means.py
@@ -1,7 +1,8 @@
 """ Test cases for the power.means module """
 
-import pytest
-from skdesign.power.means import OneSample
+# import pytest
+from skdesign.power.means import (OneSample,
+                                  TwoSampleParallel)
 
 
 def test_one_sample():
@@ -140,5 +141,130 @@ def test_one_sample():
     h = OneSample(mu=2475, mu_0=3300, stdev=663, power=0.8,
                   n=8, hypothesis="equality",
                   known_stdev=False)
+    h.calculate()
+    assert h.alpha < 0.05
+
+
+def test_two_sample_parallel():
+    """ Tests for the TwoSampleParallel class """
+
+    # From Chow et al. 3.2.5
+    h = TwoSampleParallel(mu_1=0.05, mu_2=0, stdev=0.10, alpha=0.05,
+                          power=0.8, hypothesis="equality",
+                          known_stdev=True)
+    h.calculate()
+    assert h.n_1 == 63
+    assert h.n_2 == 63
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.05, mu_2=0, stdev=0.10, alpha=0.05,
+                          n_2=63, hypothesis="equality",
+                          known_stdev=True)
+    h.calculate()
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.05, mu_2=0, stdev=0.10, n_1=63,
+                          power=0.8, hypothesis="equality",
+                          known_stdev=True)
+    h.calculate()
+    assert h.alpha < 0.05
+
+    h = TwoSampleParallel(mu_1=0.05, mu_2=0, stdev=0.10, alpha=0.05,
+                          power=0.8, hypothesis="equality",
+                          known_stdev=False)
+    h.calculate()
+    assert h.n_1 == 64
+    assert h.n_2 == 64
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.05, mu_2=0, stdev=0.10, alpha=0.05,
+                          n_2=64, hypothesis="equality",
+                          known_stdev=False)
+    h.calculate()
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.05, mu_2=0, stdev=0.10, n_2=64,
+                          power=0.8, hypothesis="equality",
+                          known_stdev=False)
+    h.calculate()
+    assert h.alpha < 0.05
+
+    h = TwoSampleParallel(mu_1=0, mu_2=0, stdev=0.10, alpha=0.05,
+                          power=0.8, hypothesis="superiority",
+                          margin=0.05, known_stdev=True)
+    h.calculate()
+    assert h.n_1 == 50
+    assert h.n_2 == 50
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0, mu_2=0, stdev=0.10, alpha=0.05,
+                          n_1=50, n_2=50, hypothesis="superiority",
+                          margin=0.05, known_stdev=True)
+    h.calculate()
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0, mu_2=0, stdev=0.10, n_2=50, ratio=1,
+                          power=0.8, hypothesis="superiority",
+                          margin=0.05, known_stdev=True)
+    h.calculate()
+    assert h.alpha < 0.05
+
+    h = TwoSampleParallel(mu_1=0, mu_2=0, stdev=0.10, alpha=0.05,
+                          power=0.8, hypothesis="superiority",
+                          margin=0.05, known_stdev=False)
+    h.calculate()
+    assert h.n_1 == 51
+    assert h.n_2 == 51
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0, mu_2=0, stdev=0.10, alpha=0.05,
+                          n_1=51, ratio=1, hypothesis="superiority",
+                          margin=0.05, known_stdev=False)
+    h.calculate()
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0, mu_2=0, stdev=0.10, n_1=51,
+                          power=0.8, hypothesis="superiority",
+                          margin=0.05, known_stdev=False)
+    h.calculate()
+    assert h.alpha < 0.05
+
+    h = TwoSampleParallel(mu_1=0.01, mu_2=0, stdev=0.10, alpha=0.05,
+                          power=0.8, hypothesis="equivalence",
+                          margin=0.05, known_stdev=True)
+    h.calculate()
+    assert h.n_1 == 108
+    assert h.n_2 == 108
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.01, mu_2=0, stdev=0.10, alpha=0.05,
+                          n_1=108, hypothesis="equivalence",
+                          margin=0.05, known_stdev=True)
+    h.calculate()
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.01, mu_2=0, stdev=0.10, n_1=108,
+                          power=0.8, hypothesis="equivalence",
+                          margin=0.05, known_stdev=True)
+    h.calculate()
+    assert h.alpha < 0.05
+
+    h = TwoSampleParallel(mu_1=0.01, mu_2=0, stdev=0.10, alpha=0.05,
+                          power=0.8, hypothesis="equivalence",
+                          margin=0.05, known_stdev=False)
+    h.calculate()
+    assert h.n_1 == 108
+    assert h.n_2 == 108
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.01, mu_2=0, stdev=0.10, alpha=0.05,
+                          n_2=108, hypothesis="equivalence",
+                          margin=0.05, known_stdev=False)
+    h.calculate()
+    assert h.power > 0.80
+
+    h = TwoSampleParallel(mu_1=0.01, mu_2=0, stdev=0.10, n_2=108,
+                          power=0.8, hypothesis="equivalence",
+                          margin=0.05, known_stdev=False)
     h.calculate()
     assert h.alpha < 0.05
