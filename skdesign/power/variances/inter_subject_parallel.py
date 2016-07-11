ef84240245941bab66a5e150650dee62c59efad3
Christopher Louden <loudenconsulting@gmail.com>
2016-07-11 16:58:32 -0500

    Add variance power calculators

---
+++ skdesign/power/variances/inter_subject_parallel.py
@@ -0,0 +1,73 @@
+from skdesign.power.means import OneSample
+import math
+
+
+class InterSubjectParallel(OneSample):
+    """ Tests for Intersubject Variablility using a Parallel study with
+    :math:`m` replicates.
+
+    The test for equality tests:
+    :math:`H_{0}: \\frac{\\sigma_{BT}}{\\sigma_{BR}} = 1` versus
+    :math:`H_{1}: \\frac{\\sigma_{BT}}{\\sigma_{BR}} \\ne 1`.
+
+    The test for superiority tests:
+    :math:`H_{0}: \\frac{\\sigma_{BT}}{\\sigma_{BR}} \\ge \\delta` versus
+    :math:`H_{1}: \\frac{\\sigma_{BT}}{\\sigma_{BR}} < \\delta`.
+
+    Attributes:
+        n: The sample size
+        m: The number of replicates
+        alpha: The :math:`\\alpha` level required by the hypothesis.
+        beta: The :math:`\\beta` level required by the hypothesis (equal to
+            :math:`1 - power`).
+        power: The power required by the hypothesis (equal to
+            :math:`1 - \\beta`).
+        stdev_wr: The variance within treatment.
+        stdev_br: The variance between replicates.
+        stdev_bt: The variance between treatment.
+        stdev_wt: The variance within replicates.
+    """
+
+    def __init__(self, n=None, m=None, stdev_bt=None, stdev_wt=None,
+                 stdev_br=None, stdev_wr=None, similarity_limit=None,
+                 hypothesis=None, alpha=None, beta=None, power=None):
+        if isinstance(m, int):
+            self.m = m
+        else:
+            raise ValueError('`m` should be of type Int.')
+
+        if hypothesis is 'superiority':
+            if similarity_limit is None:
+                raise ValueError('You must supply a `similarity_limit` for '
+                                 'this hypothesis')
+            else:
+                sigma_star = 2 * ((stdev_bt**2 + stdev_wt**2 / m)**2 +
+                                  similarity_limit**4 *
+                                  (stdev_br**2 + stdev_wr**2 / m)**2 +
+                                  (stdev_wt**4 / ((m - 1) * m**2)) +
+                                  similarity_limit**4 *
+                                  (stdev_wr**4 / ((m - 1) * m**2)))
+                sigma_star = math.sqrt(sigma_star)
+                adjusted_stdev_br = stdev_br * similarity_limit
+        elif hypothesis is 'equality':
+            sigma_star = 2 * ((stdev_bt**2 + stdev_wt**2 / m)**2 +
+                              (stdev_br**2 + stdev_wr**2 / m)**2 +
+                              (stdev_wt**4 / ((m - 1) * m**2)) +
+                              (stdev_wr**4 / ((m - 1) * m**2)))
+            sigma_star = math.sqrt(sigma_star)
+            adjusted_stdev_br = stdev_br
+        else:
+            raise ValueError("`hypothesis` should be a in "
+                             "['equality', 'superiority'].")
+
+        # Initialize the remaining arguments through the parent.
+        super(InterSubjectParallel, self).__init__(n=n,
+                                                   mu=stdev_bt**2,
+                                                   mu_0=adjusted_stdev_br**2,
+                                                   stdev=sigma_star,
+                                                   known_stdev=True,
+                                                   alpha=alpha,
+                                                   margin=0,
+                                                   beta=beta,
+                                                   power=power,
+                                                   hypothesis=hypothesis)
---
+++ skdesign/power/variances/variance_base.py
@@ -0,0 +1,71 @@
+from skdesign.power import (PowerBase,
+                            is_positive)
+
+
+class VarianceBase(PowerBase):
+    """ The base for hypothesis on variance using an F statistic.
+
+    Attributes:
+        n: The number of subjects required to test the hypothesis at an
+            :math:`\\alpha` level and a power of :math:`1 - \\beta`.
+        m: The number of replicates per subject.
+        stdev_1: The variance of the first treament.
+        stdev_2: The variance of the second treamment.
+        similarity_limit: :math:`\\delta`
+
+        alpha: The :math:`\\alpha` level required by the hypothesis.
+        beta: The :math:`\\beta` level required by the hypothesis (equal to
+            :math:`1` - power).
+        power: The power required by the hypothesis (equal to
+            :math:`1 - \\beta`).
+    """
+
+    def __init__(self, n=None, m=None, stdev_1=None, stdev_2=None,
+                 similarity_limit=None, hypothesis=None,
+                 alpha=None, beta=None, power=None):
+
+        if n is not None:
+            if isinstance(n, int):
+                self.n = n
+            else:
+                raise ValueError('`n` should be of type Int.')
+        else:
+            self.n = None
+
+        if m is not None:
+            if isinstance(m, int):
+                self.m = m
+            else:
+                raise ValueError('`m` should be of type Int.')
+        else:
+            raise ValueError('`m` must be provided.')
+
+        is_positive(stdev_1, 'stdev_1')
+        self.stdev_1 = stdev_1
+        is_positive(stdev_2, 'stdev_2')
+        self.stdev_2 = stdev_2
+
+        if hypothesis is 'equality':
+            self._alpha_adjustment = 2
+            self._beta_adjustment = 1
+            similarity_limit = 1
+            sigma_ratio = stdev_2 / stdev_1
+        elif hypothesis is 'superiority':
+            self._alpha_adjustment = 1
+            self._beta_adjustment = 1
+            if similarity_limit is None:
+                raise ValueError('A similarity_limit must be provided')
+            sigma_ratio = stdev_2 / (stdev_1 * similarity_limit)
+        elif hypothesis is 'equivalence':
+            self._alpha_adjustment = 1
+            self._beta_adjustment = 2
+            if similarity_limit is None:
+                raise ValueError('A similarity_limit must be provided')
+            sigma_ratio = (stdev_2 * similarity_limit) / stdev_1
+
+        self.similarity_limit = similarity_limit
+        self.sigma_ratio = sigma_ratio
+
+        # Initialize the remaining arguments through the parent.
+        super(VarianceBase, self).__init__(alpha=alpha, power=power,
+                                           beta=beta, hypothesis=hypothesis)
